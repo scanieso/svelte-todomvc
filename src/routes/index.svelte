eb740a02354a6e125c3c8e85b99b533578dda248
Sarah Canieso <scanieso@gmail.com>
2021-05-16 15:35:56 -0400

    Iterate over todos, add incompleted count

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,23 +1,46 @@
+<script>
+	const todos = [
+		{ id: '1', name: 'Meal prep', done: false },
+		{ id: '2', name: 'Take out recycling', done: false },
+	];
+
+	$: incompleteTodos = todos.filter((todo) => todo.done === false);
+</script>
+
 <section class="todoapp">
 	<header class="header">
 		<h1>todos</h1>
 		<input class="new-todo" placeholder="What needs to be done?" />
 	</header>
+
 	<section class="main">
-		<input id="toggle-all" class="toggle-all" type="checkbox" />
-		<label for="toggle-all">Mark all as complete</label>
+		<!-- TODO -->
+		<!-- <input id="toggle-all" class="toggle-all" type="checkbox" /> -->
+		<!-- <label for="toggle-all">Mark all as complete</label> -->
+
 		<ul class="todo-list">
-			<li>
-				<div class="view">
-					<input class="toggle" type="checkbox" />
-					<label>hello</label>
-					<button class="destroy" />
-				</div>
-			</li>
+			{#each todos as todo, i}
+				<li class={todo.done ? 'completed' : ''}>
+					<div>
+						<input
+							class="toggle"
+							type="checkbox"
+							bind:checked={todo.done}
+							id={'todo-' + todo.id} />
+						<label for="{'todo-' + todo.id}">{todo.name}</label>
+						<button class="destroy" />
+					</div>
+				</li>
+			{/each}
 		</ul>
 	</section>
+
 	<footer class="footer">
-		<span class="todo-count"><strong>1</strong> item left</span>
+		<span class="todo-count">
+			<strong>{incompleteTodos.length}</strong>
+			item{incompleteTodos.length === 1 ? '' : 's'} left
+		</span>
+
 		<ul class="filters">
 			<li>
 				<a href="#/" class="selected">All</a>
