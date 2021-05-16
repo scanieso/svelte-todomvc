0838f803d41cbd92836a16dee1151097668ce356
Sarah Canieso <scanieso@gmail.com>
2021-05-16 15:23:30 -0400

    Copy CSS and base markup from TodoMVC

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="todoapp">
+	<header class="header">
+		<h1>todos</h1>
+		<input class="new-todo" placeholder="What needs to be done?" />
+	</header>
+	<section class="main">
+		<input id="toggle-all" class="toggle-all" type="checkbox" />
+		<label for="toggle-all">Mark all as complete</label>
+		<ul class="todo-list">
+			<li>
+				<div class="view">
+					<input class="toggle" type="checkbox" />
+					<label>hello</label>
+					<button class="destroy" />
+				</div>
+			</li>
+		</ul>
+	</section>
+	<footer class="footer">
+		<span class="todo-count"><strong>1</strong> item left</span>
+		<ul class="filters">
+			<li>
+				<a href="#/" class="selected">All</a>
+			</li>
+			<li>
+				<a href="#/active">Active</a>
+			</li>
+			<li>
+				<a href="#/completed">Completed</a>
+			</li>
+		</ul>
+		<button class="clear-completed" style="display: none;" />
+	</footer>
+</section>
