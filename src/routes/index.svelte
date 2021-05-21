e44156ef0b2c7929f805129fe040268a8dad7b6c
Sarah Canieso <scanieso@gmail.com>
2021-05-20 23:08:26 -0400

    Use spaces

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,131 +1,131 @@
 <script>
-	import Filter from '../components/Filter.svelte';
+  import Filter from '../components/Filter.svelte';
 
-	let newTodoName = '';
-	let todosFilter = '';
+  let newTodoName = '';
+  let todosFilter = '';
 
-	let todos = [
-		{ name: 'One', done: false },
-		{ name: 'Two', done: true },
-		{ name: 'Three', done: false },
-	];
+  let todos = [
+    { name: 'One', done: false },
+    { name: 'Two', done: true },
+    { name: 'Three', done: false },
+  ];
 
-	const filters = [
-		{ name: 'All', filterBy: '' },
-		{ name: 'Active', filterBy: 'active' },
-		{ name: 'Completed', filterBy: 'completed' },
-	];
+  const filters = [
+    { name: 'All', filterBy: '' },
+    { name: 'Active', filterBy: 'active' },
+    { name: 'Completed', filterBy: 'completed' },
+  ];
 
-	$: visibleTodos = todos.filter((todo) => {
-		switch (todosFilter) {
-			case 'active':
-				return todo.done === false;
-			case 'completed':
-				return todo.done === true;
-			default:
-				return true;
-		}
-	});
+  $: visibleTodos = todos.filter((todo) => {
+    switch (todosFilter) {
+      case 'active':
+        return todo.done === false;
+      case 'completed':
+        return todo.done === true;
+      default:
+        return true;
+    }
+  });
 
-	$: completeTodos = todos.filter((todo) => todo.done === true);
-	$: incompleteTodos = todos.filter((todo) => todo.done === false);
+  $: completeTodos = todos.filter((todo) => todo.done === true);
+  $: incompleteTodos = todos.filter((todo) => todo.done === false);
 
-	const submitForm = (e) => {
-		e.preventDefault();
-		addTodo();
-		clearNewTodo();
-	};
+  const submitForm = (e) => {
+    e.preventDefault();
+    addTodo();
+    clearNewTodo();
+  };
 
-	const addTodo = () => {
-		if (newTodoName.trim().length === 0) {
-			return;
-		}
+  const addTodo = () => {
+    if (newTodoName.trim().length === 0) {
+      return;
+    }
 
-		todos = [
-			...todos,
-			{
-				// TODO Make ID more unique
-				id: 'todo-' + todos.length.toString(),
-				name: newTodoName,
-				done: false
-			}
-		];
-	};
+    todos = [
+      ...todos,
+      {
+        // TODO Make ID more unique
+        id: 'todo-' + todos.length.toString(),
+        name: newTodoName,
+        done: false
+      }
+    ];
+  };
 
-	const removeTodo = (id) => {
-		todos = todos.filter(todo => todo.id !== id);
-	};
+  const removeTodo = (id) => {
+    todos = todos.filter(todo => todo.id !== id);
+  };
 
-	const clearNewTodo = () => {
-		newTodoName = '';
-	};
+  const clearNewTodo = () => {
+    newTodoName = '';
+  };
 
-	const clearCompleted = () => {
-		todos = incompleteTodos;
-	};
+  const clearCompleted = () => {
+    todos = incompleteTodos;
+  };
 </script>
 
 <section class="todoapp">
-	<header class="header">
-		<h1>todos</h1>
-		<form on:submit="{submitForm}">
-			<input
-				class="new-todo"
-				placeholder="What needs to be done?"
-				bind:value={newTodoName} />
-		</form>
-	</header>
+  <header class="header">
+    <h1>todos</h1>
+    <form on:submit="{submitForm}">
+      <input
+        class="new-todo"
+        placeholder="What needs to be done?"
+        bind:value={newTodoName} />
+    </form>
+  </header>
 
-	<section class="main">
-		<!-- TODO -->
-		<!-- <input id="toggle-all" class="toggle-all" type="checkbox" /> -->
-		<!-- <label for="toggle-all">Mark all as complete</label> -->
+  <section class="main">
+    <!-- TODO -->
+    <!-- <input id="toggle-all" class="toggle-all" type="checkbox" /> -->
+    <!-- <label for="toggle-all">Mark all as complete</label> -->
 
-		<ul class="todo-list">
-			{#each visibleTodos as todo, i}
-				<li class={todo.done ? 'completed' : ''}>
-					<div>
-						<input
-							class="toggle"
-							type="checkbox"
-							bind:checked={todo.done}
-							id={'todo-' + todo.id} />
-						<label for="{'todo-' + todo.id}">{todo.name}</label>
-						<button class="destroy" on:click={() => removeTodo(todo.id)} />
-					</div>
-				</li>
-			{/each}
-		</ul>
-	</section>
+    <ul class="todo-list">
+      {#each visibleTodos as todo, i}
+        <li class={todo.done ? 'completed' : ''}>
+          <div>
+            <input
+              class="toggle"
+              type="checkbox"
+              bind:checked={todo.done}
+              id={'todo-' + todo.id} />
+            <label for="{'todo-' + todo.id}">{todo.name}</label>
+            <button class="destroy" on:click={() => removeTodo(todo.id)} />
+          </div>
+        </li>
+      {/each}
+    </ul>
+  </section>
 
-	{#if todos.length}
-		<footer class="footer">
-			<span class="todo-count">
-				<strong>{incompleteTodos.length}</strong>
-				item{incompleteTodos.length === 1 ? '' : 's'} left
-			</span>
+  {#if todos.length}
+    <footer class="footer">
+      <span class="todo-count">
+        <strong>{incompleteTodos.length}</strong>
+        item{incompleteTodos.length === 1 ? '' : 's'} left
+      </span>
 
-			<ul class="filters">
-				{#each filters as filter}
-					<li>
-						<Filter
-							on:click={() => todosFilter = filter.filterBy}
-							selectedFilter="{todosFilter}"
-							filter="{filter.filterBy}"
-						>
-							{filter.name}
-						</Filter>
-					</li>
-				{/each}
-			</ul>
+      <ul class="filters">
+        {#each filters as filter}
+          <li>
+            <Filter
+              on:click={() => todosFilter = filter.filterBy}
+              selectedFilter="{todosFilter}"
+              filter="{filter.filterBy}"
+            >
+              {filter.name}
+            </Filter>
+          </li>
+        {/each}
+      </ul>
 
-			{#if completeTodos.length > 0}
-				<button
-					class="clear-completed"
-					on:click={clearCompleted}>
-					Clear completed
-				</button>
-			{/if}
-		</footer>
-	{/if}
+      {#if completeTodos.length > 0}
+        <button
+          class="clear-completed"
+          on:click={clearCompleted}>
+          Clear completed
+        </button>
+      {/if}
+    </footer>
+  {/if}
 </section>
